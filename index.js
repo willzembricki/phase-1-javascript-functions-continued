709ae65d48dec5bb81976fa585a4bec893661f7a
William Zembricki <wzembricki@gmail.com>
2021-03-15 22:25:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+
+}
+function wrapAdjective(highlight = "*"){
+    return function(special="special"){
+        return `You are ${highlight}${special}${highlight}!`
+    }
+
+}
